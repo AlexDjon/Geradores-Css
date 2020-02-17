eaae8446db2fe7bb5c50ae4b8a07e01a3ed8b580
AlexDjon <alexdjonata30@gmail.com>
2020-02-17 17:35:55 -0300

    First Layout

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Gerador de Border Radius</title>
+
+   <!-- Meta tags -->
+   <meta name="author" content="Alex Djonata">
+   <meta name="description" content="Gerador online de Border Radius">
+   <meta name="keywords" content="css, html, border, radius, border radius, element, html">
+   <meta name="copyright" content="© 2020 Alex Djonata">
+   <meta name="theme-color" content="#222222">
+   <meta name="robots" content="index, follow">
+
+   <!-- Links -->
+   <link rel="stylesheet" href="files/css/default.css">
+   <link rel="stylesheet" href="files/css/base.css">
+   <link rel="shortcut icon" href="files/img/favicon.png">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossorigin="anonymous">
+</head>
+<body>
+   <main class='generator'>
+      <header>
+         <select id='theme-select' class='theme-select'>
+            <option selected value='black'>Dark Theme</option>
+            <option value='white'>White Theme</option>
+         </select>
+         <h3 class='title'> Border Radius Generator </h3>
+         <div class='icons'>
+            <a href="https://github.com/AlexDjon" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://twitter.com/AlexDjonata" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.facebook.com/alex.djonata.52" target="_blank"><i class="fab fa-facebook-f"></i></a>
+         </div>
+      </header>
+      <section class='container'>
+         <div class='border-radius'>
+            
+         </div>
+      </section>
+   </main>
+</body>
+</html>
